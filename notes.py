633582ef0d5ee7c6d4574ec37f970ae946e0a5de
FDULeolu <fdulyz@sina.com>
2023-09-24 16:03:18 +0800

    Store notes

---
+++ notes.py
@@ -0,0 +1,27 @@
+# CS61A Summer 2020 Lecture 6: Recursion
+# Inverse Cascade
+
+def inverse_cascade(n):
+    """
+    Write a function that prints an inverse cascade.
+
+    >>> inverse_cascade(1234)
+    1
+    12
+    123
+    1234
+    123
+    12
+    1
+    """
+    grow(n)
+    print(n)
+    shrink(n)
+
+def f_then_g(f, g, n):
+    if n:
+        f(n)
+        g(n)
+
+grow = lambda n: f_then_g(grow, print, n // 10)
+shrink = lambda n: f_then_g(print, shrink, n // 10)
