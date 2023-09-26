af84ff5335fe091816b415429e868e6b52f2653e
FDULeolu <fdulyz@sina.com>
2023-09-26 11:59:57 +0800

    Finish Discussion 05.

--- disc.py
+++ disc.py
@@ -283,3 +283,195 @@ def check_mountain_number(n):
             continue
         else: 
             return False
+        
+
+
+#################
+# Discussion 05 #
+#################  
+# Python Lists, Trees, Mutability
+
+from notes import *
+
+# 1.1
+"""
+Write a function that returns the height of a tree. Recall that the height of a tree
+is the length of the longest path from the root to a leaf.
+"""
+def height(t):
+    """
+    Return the height of a tree.
+
+    >>> t = tree(3, [tree(5, [tree(1)]), tree(2)])
+    >>> height(t)
+    2
+    """
+
+    if is_leaf(t):
+        return 0
+    else:
+        return max(height(b) for b in branches(t)) + 1
+    
+
+# 1.2
+"""
+Write a function that takes in a tree and squares every value. It should return a
+new tree. You can assume that every item is a number.
+"""
+def square_tree(t):
+    """
+    Return a tree with the square of every element in t
+    >>> numbers = tree(1,
+    ...                [tree(2,
+    ...                      [tree(3),
+    ...                       tree(4)]),
+    ...                 tree(5,
+    ...                      [tree(6,
+    ...                            [tree(7)]),
+    ...                       tree(8)])])
+    >>> print_tree(square_tree(numbers))
+    1
+     4
+      9
+      16
+     25
+      36
+       49
+      64
+    """
+
+    if is_leaf(t):
+        return tree(label(t) ** 2)
+    else:
+        return tree(label(t) ** 2, [square_tree(b) for b in branches(t)])
+    
+
+# 1.3
+"""
+Write a function that takes in a tree and a value x and returns a list containing the
+nodes along the path required to get from the root of the tree to a node containing
+x.
+If x is not present in the tree, return None. Assume that the entries of the tree are
+unique.
+"""
+def find_path(tree, x):
+    if label(tree) == x:
+        return [label(tree)]
+    elif not is_leaf(tree):
+        path = [label(tree)]
+        for b in branches(tree):
+            subpath = find_path(b, x)
+            if subpath:
+                return path + subpath
+        return None
+    else:
+        return None
+
+
+# 2.2
+"""
+Write a function that takes in a value x, a value el, and a list and adds as many
+el’s to the end of the list as there are x’s. Make sure to modify the original
+list using list mutation techniques.
+"""
+def add_this_many(x, el, lst):
+    """ 
+    Adds el to the end of lst the number of times x occurs in lst.
+
+    >>> lst = [1, 2, 4, 2, 1]
+    >>> add_this_many(1, 5, lst)
+    >>> lst
+    [1, 2, 4, 2, 1, 5]
+    >>> add_this_many(2, 2, lst)
+    >>> lst
+    [1, 2, 4, 2, 1, 5, 2, 2]
+    """
+
+    while x > 0:
+        lst.append(el)
+        x -= 1
+
+
+# 2.3
+"""
+Write a function that takes in a sequence s and a function fn and returns a dictionary.
+The values of the dictionary are lists of elements from s. Each element e in a list
+should be constructed such that fn(e) is the same for all elements in that list.
+Finally, the key for each value should be fn(e).
+"""
+def group_by(s, fn):
+    """
+    >>> group_by([12, 23, 14, 45], lambda p: p // 10)
+    {1: [12, 14], 2: [23], 4: [45]}
+    >>> group_by(range(-3, 4), lambda x: x * x)
+    {0: [0], 1: [-1, 1], 4: [-2, 2], 9: [-3, 3]}
+    """
+
+    result = {}
+    for num in s:
+        if fn(num) not in result:
+            result[fn(num)] = [num]
+        else:
+            result[fn(num)].append(num)
+
+    sorted_keys = sorted(result.keys())
+    sorted_dict = {key: result[key] for key in sorted_keys}
+
+    return sorted_dict
+
+
+# Quiz 1 So Many Options
+# (a)
+"""
+Implement the following function partition_options which outputs all the ways to partition a number total using numbers no larger than biggest.
+"""
+def partition_options(total, biggest):
+    """
+    >>> partition_options(2, 2)
+    [[2], [1, 1]]
+    >>> partition_options(3, 3)
+    [[3], [2, 1], [1, 1, 1]]
+    >>> partition_options(4, 3)
+    [[3, 1], [2, 2], [2, 1, 1], [1, 1, 1, 1]]
+    """
+
+    if total == 0:
+        return [[]]
+    elif total < 0 or 1 > biggest:
+        return []
+    else:
+        with_biggest = partition_options(total - biggest, biggest)
+        without_biggest = partition_options(total, biggest - 1)
+
+        with_biggest = [[biggest] + sub_options for sub_options in with_biggest]
+        
+        return with_biggest + without_biggest
+    
+
+# (b)
+"""
+Return the minimum number of elements from the list that need to be summed in order to add up to T.
+The same element can be used multiple times in the sum. For example, for T = 11 and lst = [5, 4, 1] we should return 3 because at minimum we need to add 3 numbers together (5, 5, and 1). You can assume that there always exists a linear combination of the elements in lst that equals T.
+"""
+def min_elements(T, lst):
+    """
+    >>> min_elements(10, [4, 2, 1]) # 4 + 4 + 2
+    3
+    >>> min_elements(12, [9, 4, 1]) # 4 + 4 + 4
+    3
+    >>> min_elements(0, [1, 2, 3])
+    0
+    """
+    # 如果目标值 T 等于 0，则不需要添加任何元素
+    if T == 0:
+        return 0
+    # 如果目标值 T 小于 0 或列表为空，返回无穷大表示不可行
+    if T < 0 or not lst:
+        return float('inf')
+    
+    # 递归计算包含当前元素和不包含当前元素两种情况下的最小元素数量
+    with_current = 1 + min_elements(T - lst[0], lst)
+    without_current = min_elements(T, lst[1:])
+    
+    # 返回两种情况下的最小值
+    return min(with_current, without_current)
