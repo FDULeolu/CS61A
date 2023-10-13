66149db57b686c1c0be08e7aef48ac75b0597a33
FDULeolu <fdulyz@sina.com>
2023-10-13 15:39:45 +0800

    Finish hw 07.

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -2,47 +2,78 @@
   (cdr (cdr s)))
 
 (define (cadr s)
-  'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
-  'YOUR-CODE-HERE
+  (car (cdr (cdr s)))
 )
 
 
 (define (sign num)
-  'YOUR-CODE-HERE
+  (cond
+    ((> num 0) 1)
+    ((< num 0) -1)
+    ((= num 0) 0))
 )
 
 
 (define (square x) (* x x))
 
 (define (pow x y)
-  'YOUR-CODE-HERE
+  ; (if (= y 0) 1)
+  ; (if (= y 1) x
+  ;   (if (even? y)
+  ;     (square (pow x (/ y 2))))
+  ;     (* x (square (pow x (floor (/ y 2)))))
+  ; )
+  (if (= y 1) x
+        (if (even? y) 
+            (square (pow x (/ y 2)))
+            (* x (square (pow x (floor (/ y 2)))))
+        )
+    )
 )
 
 
 (define (unique s)
-  'YOUR-CODE-HERE
+  (if (null? s) s
+  (cons (car s) (unique (filter (lambda (x) (not(equal? x (car s))) ) (cdr s))))
+  )
 )
 
 
 (define (replicate x n)
-  'YOUR-CODE-HERE
+  (define (replicate_tail_recursive x s n)
+    (if (= n 0) s
+      (replicate_tail_recursive x (cons x s) (- n 1))
+      )
+    )
+  (replicate_tail_recursive x nil n)
   )
 
 
 (define (accumulate combiner start n term)
-  'YOUR-CODE-HERE
+  (define (accumu_helper combiner start n term)
+    (if (= 0 n) start
+      (accumu_helper combiner (combiner start (term n)) (- n 1) term)
+      )
+    )
+  (accumu_helper combiner start n term)
 )
 
-
+; I don't want to rewrite the traditional recursive func again...
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
+  (define (accumu_helper combiner start n term)
+    (if (= 0 n) start
+      (accumu_helper combiner (combiner start (term n)) (- n 1) term)
+      )
+    )
+  (accumu_helper combiner start n term)
 )
 
 
 (define-macro (list-of map-expr for var in lst if filter-expr)
-  'YOUR-CODE-HERE
+  `(map (lambda (,var) ,map-expr) (filter (lambda (,var) ,filter-expr) ,lst))
 )
 
