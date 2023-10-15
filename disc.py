18bde5f3600e7f5788e48cae08e17275041d60a2
FDULeolu <fdulyz@sina.com>
2023-10-15 21:27:05 +0800

    Update project scheme question 9.

--- disc.py
+++ disc.py
@@ -1133,3 +1133,26 @@ def alt_tree_map(t, map_fn):
 #####################################
 
 # Finish questions in scm file.
+
+
+##########################################
+# Discussion 11 Tail Recursion and Macros#
+##########################################
+
+# 2.1
+"""Write a “macro” in Python called make lambda that takes in two parameters: params, a string containing a comma-separated list of variable names; and body, a Python expression in string form, and returns a lambda function that takes params as its parameters and body as its body."""
+
+def make_lambda(params, body):
+    """
+    >>> f = make_lambda("x, y", "x + y")
+    >>> f
+    <function <lambda> at ...>
+    >>> f(1, 2)
+    3
+    >>> g = make_lambda("a, b, c", "c if a > b else -c")
+    >>> g(1, 2, 3)
+    -3
+    >>> make_lambda("f, x, y", "f(x, y)")(f, 1, 2)
+    3
+    """
+    return eval("lambda" + params + ":" + body)
